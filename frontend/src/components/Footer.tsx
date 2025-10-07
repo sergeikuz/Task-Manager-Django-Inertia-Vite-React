6532c93b48080dae92fcf0ea2dc5f52cefe9cb34
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2025-10-07 19:21:59 +0300

    update(name)

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="card-footer text-body-secondary border-top pt-4">
-                <p className="text-center">© Shomin Daniil 2025</p>
+                <p className="text-center">© Sergei Kuznetsov 2025</p>
             </div>
         </footer>
     );
