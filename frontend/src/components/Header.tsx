4ce3867717e338de6c1c1d98508b6ca5b04fbbd2
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2025-10-07 19:22:45 +0300

    update: Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,63 +3,63 @@ import {Link} from '@inertiajs/react';
 import { If, Then, Else, Fallback } from 'react-if';
 import { usePage } from '@inertiajs/react';
 
-function Header( ) {
+function Header() {
     const { props } = usePage();
     const { auth } = props;
+
     return (
         <header>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                <Link className="navbar-brand" href="/">Task manager</Link>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link className="nav-link" href="/users/">Users</Link>
-                    </li>
-                    { auth?.isAuthenticated && (
-                        <div className="nav-item">
+                    <Link className="navbar-brand" href="/">Task manager</Link>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        
+                        <ul className="navbar-nav me-auto d-flex align-items-center" style={{ flexWrap: 'nowrap' }}>
                             <li className="nav-item">
-                                <Link className="nav-link" href="/statuses/">Statuses</Link>
+                                <Link className="nav-link" href="/users/">Users</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="/labels/">Labels</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="/tasks/">Tasks</Link>
-                            </li>
-                        </div>
-                    )}
-                    </ul>
-                    <ul className="navbar-nav">
-                    { auth?.isAuthenticated ? (
-                        <div className="nav-item">
-                            <li className="nav-item">
-                                <Link 
-                                    href="/logout/" 
-                                    method="post" 
-                                    as="button" 
-                                    className="btn btn-outline-primary ms-2"
-                                >
-                                    Exit
-                                </Link>
-                            </li>
-                        </div>
-                    ) : (
-                        <div>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="/login/">Login</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="/users/create/">Registration</Link>
-                            </li>
-                        </div>
-                    )}           
-                    </ul>
-                </div>
+                            {auth?.isAuthenticated && (
+                                <>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" href="/statuses/">Statuses</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" href="/labels/">Labels</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" href="/tasks/">Tasks</Link>
+                                    </li>
+                                </>
+                            )}
+                        </ul>
+                        
+                        <ul className="navbar-nav ms-auto d-flex align-items-center" style={{ flexWrap: 'nowrap' }}>
+                            {auth?.isAuthenticated ? (
+                                <li className="nav-item">
+                                    <Link 
+                                        href="/logout/" 
+                                        method="post" 
+                                        as="button" 
+                                        className="btn btn-outline-primary ms-2"
+                                    >
+                                        Exit
+                                    </Link>
+                                </li>
+                            ) : (
+                                <>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" href="/login/">Login</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" href="/users/create/">Registration</Link>
+                                    </li>
+                                </>
+                            )}
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </header>
-        
     );
 }
 
