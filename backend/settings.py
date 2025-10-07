a58c4c4fcc6b45e2e14e19dc5f52c94fe0bb1bb3
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2025-10-07 19:20:28 +0300

    add: middleware(share-inertiajs)

--- backend/settings.py
+++ backend/settings.py
@@ -87,15 +87,15 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
     'inertia.middleware.InertiaMiddleware',
-    #'backend.middleware.InertiaMiddleware',
+    'backend.middleware.InertiaMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
